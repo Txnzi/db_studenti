6a3c5b48e6b9e1fad24d23f46881835d6395f995
francescocalabrese2005-spec <francesco.calabrese.2005@gmail.com>
2026-02-23 17:43:27 +0100

    importato file studenti

---
+++ models/studenti.py
@@ -0,0 +1,20 @@
+class Studenti:
+
+    __id: int
+    nome: str
+    cognome: str
+    eta: int
+    voti: list[int]
+
+
+    @property
+    def id(self):
+        return self.__id
+
+
+    def __init__(self,id: int,nome:str,cognome: str,eta: int,voti: list[int]):
+        self.id= id
+        self.nome= nome
+        self.cognome= cognome
+        self.eta= eta
+        self.voti= voti
