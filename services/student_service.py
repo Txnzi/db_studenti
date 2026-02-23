ec5b8806a358139adb67168c22401561bdfcc21e
francescocalabrese2005-spec <francesco.calabrese.2005@gmail.com>
2026-02-23 18:42:23 +0100

    creato student_service

---
+++ services/student_service.py
@@ -0,0 +1,23 @@
+from database.student_database import StudentDatabase
+from models.studenti import Studenti
+class StudentService:
+    
+
+    def leggi(db : StudentDatabase):
+        pass
+
+
+    def scrivi(db : StudentDatabase, nuovo_studente : Studenti):
+        pass
+
+
+    def modifica(studente : Studenti, db : StudentDatabase, id : int):
+        pass
+
+
+    def elimina(db : StudentDatabase, id : int):
+        pass
+
+
+    def leggi_dettaglio(db : StudentDatabase, id : int):
+        pass
