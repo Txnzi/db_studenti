78414e5df7333dcfa0b5fdefeeae188a50df9543
Txzni <fralorusso02@gmail.com>
2026-02-24 17:45:09 +0100

    scappellate varie

--- services/student_service.py
+++ services/student_service.py
@@ -8,12 +8,24 @@ class StudentService:
         pass
 
 
-    def scrivi(db : StudentDatabase, nuovo_studente : Studenti):      #txnzy
-        pass
+    def scrivi(db : StudentDatabase, sd : dict):      #txnzy
+        db.apri()
+        nuovo_studente=Studenti(sd["id"],sd["nome"],sd["cognome"],sd["eta"],sd["voti"])
+        db.file.append(nuovo_studente)
+        db.chiudi()
 
 
-    def modifica(studente : Studenti, db : StudentDatabase, id : int):   #txnzy
-        pass
+    def modifica(student : dict, db : StudentDatabase, id_stud : int):   #txnzy
+        db.apri()
+        for el in db.file:
+            if el.id == id_stud:
+                el.nome=student["nome"]
+                el.cognome=student["cognome"]
+                el.eta=student["eta"]
+                el.voti=student["voti"]
+        db.chiudi()
+
+
 
 
     def elimina(db : StudentDatabase, id : int):    #calab
