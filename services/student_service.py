b472d818a88bec8e249f1ed233c2d5c9b4fad417
francescocalabrese2005-spec <francesco.calabrese.2005@gmail.com>
2026-02-24 16:54:35 +0100

    commenti in student_service

--- services/student_service.py
+++ services/student_service.py
@@ -3,23 +3,23 @@ from models.studenti import Studenti
 class StudentService:
     
 
-    def leggi(db : StudentDatabase):
+    def leggi(db : StudentDatabase):       #calab
         print()
         pass
 
 
-    def scrivi(db : StudentDatabase, nuovo_studente : Studenti):
+    def scrivi(db : StudentDatabase, nuovo_studente : Studenti):      #txnzy
         pass
 
 
-    def modifica(studente : Studenti, db : StudentDatabase, id : int):
+    def modifica(studente : Studenti, db : StudentDatabase, id : int):   #txnzy
         pass
 
 
-    def elimina(db : StudentDatabase, id : int):
+    def elimina(db : StudentDatabase, id : int):    #calab
         pass
 
-
-    def leggi_dettaglio(db : StudentDatabase, id : int):
-        print()
+  
+    def leggi_dettaglio(db : StudentDatabase, id : int):   #calab
+        print() 
         pass
